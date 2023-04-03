186e2391f5a33d2ec7ef1860935896c257cf330b
tavopaz <tavo.paz12.dev@hotmail.com>
2023-04-03 16:54:09 -0600

    feauture: ui modificada

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,44 +1,48 @@
-import image from '../assets/images/banner.webp'
+import image from "../assets/images/banner.webp";
 
-import '../styles/About/About.scss'
+import "../styles/About/About.scss";
 
-export default function About () {
+export default function About() {
   return (
-    <section className='about'>
-      <h2 className='title'>Sobre mi...</h2>
+    <section className="about">
+      <h2 className="title">Sobre mi...</h2>
 
-      <div className='about__card'>
-        <div className='image'>
-          <img src={image} alt='about' loading='lazy' />
+      <div className="about__card">
+        <div className="image">
+          <img src={image} alt="about" loading="lazy" />
         </div>
-        <div className='biografia'>
+        <div className="biografia">
           <p>
-            ¡Hola! Mi nombre es José Octavio Paz y soy desarrollador frontend
-            con más de 3 años de experiencia. Me especializo en React.js,
-            LitElement, JavaScript, HTML5 y CSS3, lo que me permite crear
-            soluciones de software innovadoras y de alta calidad. También tengo
-            una sólida comprensión de los principios de diseño de UI/UX, CSS
-            avanzado (Módulos CSS, SCSS) y diseño receptivo.
+            Soy un desarrollador frontend altamente especializado con más de 2
+            años de experiencia en el uso de tecnologías como React.js,
+            LitElement, JavaScript, HTML5 y CSS3 para crear soluciones de
+            software innovadoras y de alta calidad. Mi enfoque se centra en
+            diseñar interfaces de usuario atractivas y fáciles de usar que
+            mejoren la experiencia del usuario.
           </p>
           <p>
-            Pero ser desarrollador es algo más que escribir código. Se trata de
-            crear soluciones que resuelvan problemas reales y tengan un impacto
-            positivo en la vida de las personas. Es por eso que me apasiona lo
-            que hago y siempre me esfuerzo por estar al día con las últimas
-            tendencias tecnológicas y mejorar mis habilidades y conocimientos en
-            el campo.
+            Un ejemplo de mi trabajo más reciente fue durante mis residencias
+            profesionales, donde tuve la oportunidad de desarrollar una
+            plataforma de e-learning desde cero, desde la ideación hasta la
+            implementación, para asegurarme de que cada aspecto de la plataforma
+            cumpliera con los requisitos y necesidades del cliente. Además,
+            desarrollé la API utilizando tecnologías como Node.js, Express,
+            Postgres y Docker, configuré el servidor de la aplicación para
+            asegurar una comunicación fluida entre el front-end y la API.
+            Gracias a mi experiencia técnica y dedicación, pudimos lanzar la
+            plataforma de e-learning en tiempo y forma, lo que resultó en una
+            tasa de adopción del 85% entre los estudiantes.
           </p>
           <p>
-            Estoy comprometido a escribir código de calidad y crear experiencias
-            de usuario excepcionales. Con una actitud positiva y un enfoque en
-            agregar valor, siempre estoy listo para asumir nuevos desafíos y
-            trabajar con equipos de desarrollo para brindar resultados
-            sobresalientes. Si está buscando un desarrollador apasionado y
-            dedicado para llevar su proyecto al siguiente nivel, no dude en
-            ponerse en contacto.
+            Además de mi trabajo, disfruto de la música y el senderismo en mi
+            tiempo libre. Me apasiona lo que hago y siempre me esfuerzo por
+            estar al día con las últimas tendencias tecnológicas y mejorar mis
+            habilidades y conocimientos en el campo. Si está buscando un
+            desarrollador apasionado y comprometido para llevar su proyecto al
+            siguiente nivel, no dude en ponerse en contacto conmigo
           </p>
         </div>
       </div>
     </section>
-  )
+  );
 }
